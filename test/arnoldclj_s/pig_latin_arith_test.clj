e18a3ca851bf259d91d6fa77817e85480f358969
Juan Vazquez <juanvazquez+git@gmail.com>
2017-02-24 12:46:40 -0600

    fixing formatting

--- test/arnoldclj_s/pig_latin_arith_test.clj
+++ test/arnoldclj_s/pig_latin_arith_test.clj
@@ -2,33 +2,32 @@
   (:use midje.sweet)
   (:use arnoldclj_s.pig-latin-lexr)) 
 
-
-
 ;original string
 #_"IT'S SHOWTIME 
- HEY CHRISTMAS TREE VAR 
-                          YOU SET US UP 22 
-                          GET TO THE CHOPPER VAR 
-                          HERE IS MY INVITATION 11 
-                          GET DOWN 43 
-                          GET UP 54 
-                          GET UP 44 
-                          ENOUGH TALK 
-                          TALK TO THE HAND VAR 
-                          YOU HAVE BEEN TERMINATED"
+  HEY CHRISTMAS TREE VAR 
+  YOU SET US UP 22 
+  GET TO THE CHOPPER VAR 
+  HERE IS MY INVITATION 11 
+  GET DOWN 43 
+  GET UP 54 
+  GET UP 44 
+  ENOUGH TALK 
+  TALK TO THE HAND VAR 
+  YOU HAVE BEEN TERMINATED"
+
 (fact "evaluate using different arithmetic operations"
-  ( ights-camera-actionlay
-"IT'SWAY OWTIMESHAY 
- EYHAY ISTMASCHRAY EETRAY ARVAY 
- OUYAY ETSAY USWAY UPWAY 22 
- ETGAY OTAY ETHAY OPPERCHAY ARVAY 
- EREHAY ISWAY MYAY INVITATIONWAY 11
- ETGAY OWNDAY 43
- ETGAY UPWAY 54
- ETGAY UPWAY 44
- ENOUGHWAY ALKTAY 
- ALKTAY OTAY ETHAY ANDHAY ARVAY 
- OUYAY AVEHAY EENBAY ERMINATEDTAY" 
+  (ights-camera-actionlay
+     "IT'SWAY OWTIMESHAY 
+      EYHAY ISTMASCHRAY EETRAY ARVAY 
+      OUYAY ETSAY USWAY UPWAY 22 
+      ETGAY OTAY ETHAY OPPERCHAY ARVAY 
+      EREHAY ISWAY MYAY INVITATIONWAY 11
+      ETGAY OWNDAY 43
+      ETGAY UPWAY 54
+      ETGAY UPWAY 44
+      ENOUGHWAY ALKTAY 
+      ALKTAY OTAY ETHAY ANDHAY ARVAY 
+      OUYAY AVEHAY EENBAY ERMINATEDTAY" 
 ) => 
 [:Program 
  [:begin-main 
